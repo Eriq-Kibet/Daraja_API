8b9e672c86a3f1648ff690facf2a8f5b9721a979
Eric <eric2017kibet@gmail.com>
2021-12-09 03:00:06 -0500

    stk, b2c improvement

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 const port = 7080;
 const consumerKey = "p5lDJyvTDdnnhTsXPjGrkpXeq67R3Wr0";
 const consumerSecret = "491N3hNKFpDPYMGt";
-const shortCode = 600990;
+const shortCode = "600991";
 
 // Routes
 app.get("/", (req, res) => {
@@ -35,8 +35,8 @@ app.get("/register", access, (req, res) => {
       json: {
         ShortCode: shortCode,
         ResponseType: "Completed",
-        ConfirmationURL: "https://178.79.144.23/confirmation",
-        ValidationURL: "https://178.79.144.23/validation"
+        ConfirmationURL: "https://mydomain.com/confirmation",
+        ValidationURL: "https://mydomain.com/validation"
       }
     },
     (error, response, body) => {
@@ -63,7 +63,7 @@ app.post("/validation", access, (req, res) => {
   console.log(res);
 });
 
-// Simulate lipa na Mpesa or Paybell..
+// Simulate lipa na Mpesa or Paybill..
 
 app.get("/simulate", access, (req, res) => {
   const url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate";
@@ -80,7 +80,7 @@ app.get("/simulate", access, (req, res) => {
         Amount: "10",
         Msisdn: "254708374149",
         BillRefNumber: "TestAPI",
-        ShortCode: shortCode.toString()
+        ShortCode: shortCode
       }
     },
     (error, response, body) => {
@@ -110,8 +110,8 @@ app.get("/balance", access, (req, res) => {
         Initiator: "testapi",
         SecurityCredential:
           "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
-        QueueTimeOutURL: "https://Your public IP/AccountBalance/queue/",
-        ResultURL: "https://Your public IP/AccountBalance/result/"
+        QueueTimeOutURL: "https://mydomain.com/AccountBalance/queue",
+        ResultURL: "https://mydomain.com/AccountBalance/result"
       }
     },
     (error, response, body) => {
@@ -122,25 +122,49 @@ app.get("/balance", access, (req, res) => {
     }
   );
 });
+//simulation Result
 
-app.post("/queue", access, (req, res) => {
-  console.log(".............queue.................");
+app.post("/AccountBalance/queue", access, (req, res) => {
+  console.log(".............AccountBalance queue.................");
   res.send(req.body);
   console.log(req.body);
 });
 
-app.post("/result", access, (req, res) => {
+app.post("/AccountBalance/result", access, (req, res) => {
   res.send(req.body);
-  console.log(".............result.................");
+  console.log(".............AccountBalance result.................");
   console.log(req.body);
   console.log(res);
 });
 
 // Stk request phone to pay
 app.get("/stk", access, (req, res) => {
-  const url = "";
+  const url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
   const auth = "Bearer " + req.access_token;
 
+  let date = new Date();
+  const timestamp =
+    date.getFullYear() +
+    "" +
+    "" +
+    date.getMonth() +
+    "" +
+    "" +
+    date.getDate() +
+    "" +
+    "" +
+    date.getHours() +
+    "" +
+    "" +
+    date.getMinutes() +
+    "" +
+    "" +
+    date.getSeconds();
+  const password = new Buffer.from(
+    "174379" +
+      "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919" +
+      timestamp
+  ).toString("base64");
   request(
     {
       url: url,
@@ -148,7 +172,19 @@ app.get("/stk", access, (req, res) => {
       headers: {
         Authorization: "Basic " + auth
       },
-      json: {}
+      json: {
+        BusinessShortCode: "174379",
+        Password: password,
+        Timestamp: timestamp,
+        TransactionType: "CustomerPayBillOnline",
+        Amount: "1",
+        PartyA: "254708374149",
+        PartyB: "174379",
+        PhoneNumber: "254708374149",
+        CallBackURL: "https://mydomain.com/stk_callback",
+        AccountReference: "CompanyXLTD",
+        TransactionDesc: "Payment of X"
+      }
     },
     (error, response, body) => {
       if (error) {
@@ -159,9 +195,15 @@ app.get("/stk", access, (req, res) => {
   );
 });
 
+//STK response
+app.post("/stk_callback", (req, res) => {
+  console.log(".......... STK Callback ..................");
+  console.log(JSON.stringify(req.body.Body.stkCallback));
+});
+
 // Business to Transaction
 app.get("/b2c", access, (req, res) => {
-  const url = "";
+  const url = "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest";
   const auth = "Bearer " + req.access_token;
 
   request(
@@ -171,7 +213,19 @@ app.get("/b2c", access, (req, res) => {
       headers: {
         Authorization: "Basic " + auth
       },
-      json: {}
+      json: {
+        InitiatorName: "testapi",
+        SecurityCredential:
+          "SLobAnHr19DCtX69AYBdInop1pT3kdnwaDqBxu2xgZ2WhpznCzAqdrvdL032Qjr/RbTIHGIRZMVmqwUjMCP2s/3IZzTv2QYvXQrKqinBPPpq7la/CCkbHH34lWN/YUZDIDEpwVbjTiILfcIQ4EuuJlCSnuw4sGmtOUgtwZRDuYDSvPfUbpJc+xcdnoIm/wVtFbvUEZWJwwPv48wjp6XEDPfKZZqPW6rFcha5LztZW3n4bYxiBNvS7tnX0qsEr3RXaahsnI3B+trhAdpTBH2zForWw50FsPrqqMdJ5reulOUmPrICt8AaxOG3QFI6BybUpIrNncvUeIZF6zTma9tJEQ==",
+        CommandID: "BusinessPayment",
+        Amount: "1",
+        PartyA: "600981",
+        PartyB: "254708374149",
+        Remarks: "Test remarks ",
+        QueueTimeOutURL: "https://mydomain.com/b2c/queue",
+        ResultURL: "https://mydomain.com/b2c/result",
+        Occassion: "Sent Wrongly"
+      }
     },
     (error, response, body) => {
       if (error) {
@@ -180,6 +234,17 @@ app.get("/b2c", access, (req, res) => {
       res.status(200).json(body);
     }
   );
+});
+
+//B2C REsponse
+app.post("/b2c/result", (req, res) => {
+  console.log("-------------------- B2C Result -----------------");
+  console.log(JSON.stringify(req.body.Result));
+});
+
+app.post("/b2c/queue", (req, res) => {
+  console.log("-------------------- B2C Timeout -----------------");
+  console.log(req.body);
 });
 
 //generating access tokens
